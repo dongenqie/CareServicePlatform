324077c63819df740a0163bda2b82e839346d231
dongenqie <2047957247@qq.com>
2025-06-26 08:25:15 +0800

    初始化提交

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h2>欢迎来到主页面</h2>
+    <p>你已经成功登录！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
